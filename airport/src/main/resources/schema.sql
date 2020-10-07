effd9bb068466b074fb8ac5d62d8048e09d2c19f
zaxar396 <zaxar396@yandex.ru>
2020-10-07 20:50:17 +0300

    Update schema.sql

--- airport/src/main/resources/schema.sql
+++ airport/src/main/resources/schema.sql
@@ -1,42 +1,42 @@
 drop schema if exists PLANE_SCHEDULER cascade;
 
 create schema PLANE_SCHEDULER;
-set schema PLANE_SCHEDULER;
+set schema 'PLANE_SCHEDULER';
 
-create table PLANE
+create table PLANE_SCHEDULER.PLANE
 (
-    ID          identity not null primary key,
-    DESCRIPTION varchar2(255),
+    ID          integer not null primary key generated always as identity,
+    DESCRIPTION varchar(255),
     MAX_SPEED   integer  not null
 );
 
-create table AIRPORT
+create table PLANE_SCHEDULER.AIRPORT
 (
-    ID          identity      not null primary key,
-    DESCRIPTION varchar2(255),
+    ID          integer not null primary key generated always as identity,
+    DESCRIPTION varchar(255),
     CAPACITY    integer default 0,
     LATITUDE    decimal(9, 6) not null,
     LONGITUDE   decimal(9, 6) not null
 
 );
 
-create table ROUTE
+create table PLANE_SCHEDULER.ROUTE
 (
-    ID      identity not null primary key,
+    ID      integer not null primary key generated always as identity,
     PLANE_ID integer  not null,
 
-    foreign key (PLANE_ID) references PLANE
+    foreign key (PLANE_ID) references PLANE_SCHEDULER.PLANE
 );
 
-create table ROUTE_POINT
+create table PLANE_SCHEDULER.ROUTE_POINT
 (
-    ID                identity not null primary key,
+    ID                integer not null primary key generated always as identity,
     ROUTE_ID          integer  not null,
     AIRPORT_ID       integer  not null,
     ROUTE_POINT_INDEX integer  not null,
     DEPART_TIME       timestamp with time zone,
     ARRIVE_TIME       timestamp with time zone,
 
-    foreign key (ROUTE_ID) references ROUTE,
-    foreign key (AIRPORT_ID) references AIRPORT
+    foreign key (ROUTE_ID) references PLANE_SCHEDULER.ROUTE,
+    foreign key (AIRPORT_ID) references PLANE_SCHEDULER.AIRPORT
 );
